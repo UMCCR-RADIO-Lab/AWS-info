0d1e441576af98df900dd39a2e40b4da40995261
AndrewPattison <patto333333@gmail.com>
2020-08-03 15:25:00 +1000

    made the pileup more pipeline friendly

--- facets-analysis/snp_pileup.sh
+++ facets-analysis/snp_pileup.sh
@@ -6,21 +6,30 @@
 # Bams/snp ref should be in here:
 #s3://umccr-temp-dev/Andrew_parallel_cluster_test/
 
-# BCBio has output files in the wrong order that FACETS wants for some reason. Picard reorder to fix this 
-picard ReorderSam I=PRJ190493_FL-2T-ready.bam O=PRJ190493_FL-2T_chr_ordered.bam R=hg38.fa
-picard ReorderSam I=PRJ190494_FL-2B-ready.bam O=PRJ190494_FL-2B_chr_ordered.bam R=hg38.fa
+# Example command
+# bash snp_pileup.sh hg38.fa blood.bam tumour.bam ouput_dir snp_reference snp_pileup_program
 
-outdir=./output
+# hg38.fa etc
+genome_ref=$1
+#PRJ190494_FL-2B.bam
+BCBio_blood_bam=$2
+# PRJ190493_FL-2T.bam
+BCBio_tumour_bam=$3
+# Output directory
+outdir=$4
 # Ref files are from here as suggested in the FACETS docs ftp://ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606_b150_GRCh38p7/VCF/
-SNP_reference=00-All.vcf.gz
+#00-All.vcf.gz
+SNP_reference=$5
+# SNP pileup execuatble
+snp_pileup=$6
+
+# BCBio has output files in the wrong order that FACETS wants for some reason. Picard reorder to fix this 
+picard ReorderSam I=$BCBio_tumour_bam O=$outdir/$BCBio_tumour_bam.ordered.bam R=$genome_ref
+picard ReorderSam I=$BCBio_blood_bam O=$outdir/$BCBio_blood_bam.ordered.bam R=$genome_ref
+
 mkdir -p $outdir
 
-# Tumour bam
-bam_file_t=PRJ190493_FL-2T_chr_ordered.bam
-# Normal (blood) bam
-bam_file_n=PRJ190494_FL-2B_chr_ordered.bam
-
 # SNP pileup from here: https://github.com/mskcc/facets/tree/master/inst/extcode
-snp-pileup -g -q 30 -Q 30 -r 10,10 $SNP_reference $outdir/$tumour.pileup.gz $bam_file_n $bam_file_t
+$snp_pileup -g -q 30 -Q 30 -r 10,10 $SNP_reference $outdir/$BCBio_tumour_bam.csv.gz $outdir/$BCBio_blood_bam.ordered.bam $outdir/$BCBio_tumour_bam.ordered.bam
 
 
