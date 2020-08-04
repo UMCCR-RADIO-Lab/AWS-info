bc315a5e0076b6efeed1932939231ce23cfcf9ef
Roman Valls Guimera <brainstorm@nopcode.org>
2020-08-04 15:48:48 +1000

    OOS condition on picard because of TMP_DIR, basename w/ correct syntax

--- facets-analysis/snp_pileup.sh
+++ facets-analysis/snp_pileup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
-docker_cmd_prefix="docker run -it -v /mnt/data:/mnt/data -v /mnt/refdata:/mnt/refdata -v /mnt/out:/mnt/out"
+export TMP_DIR="/scratch"
+docker_cmd_prefix="docker run -it -v /scratch:/scratch -v /mnt/data:/mnt/data -v /mnt/refdata:/mnt/refdata -v /mnt/out:/mnt/out --env TMP_DIR"
 picard="$docker_cmd_prefix quay.io/biocontainers/picard:2.23.3--0 picard"
 snp_pileup="$docker_cmd_prefix quay.io/biocontainers/snp-pileup:0.5.14--hfbaaabd_3 snp-pileup"
 
@@ -19,8 +20,8 @@ outdir=$4
 SNP_reference=$5
 
 # BCBio has output files in the wrong order that FACETS wants for some reason. Picard reorder to fix this
-$picard ReorderSam -I $BCBio_tumour_bam -O $outdir/`basename($BCBio_tumour_bam)`.ordered.bam -R $genome_ref -SD /mnt/refdata/genomes_1.7/GRCh37/GRCh37.dict
-$picard ReorderSam -I $BCBio_blood_bam -O $outdir/`basename($BCBio_blood_bam)`.ordered.bam -R $genome_ref -SD /mnt/refdata/genomes_1.7/GRCh37/GRCh37.dict
+$picard ReorderSam -I $BCBio_tumour_bam -O $outdir/`basename $BCBio_tumour_bam`.ordered.bam -R $genome_ref -SD /mnt/refdata/genomes_1.7/GRCh37/GRCh37.dict
+$picard ReorderSam -I $BCBio_blood_bam -O $outdir/`basename $BCBio_blood_bam`.ordered.bam -R $genome_ref -SD /mnt/refdata/genomes_1.7/GRCh37/GRCh37.dict
 
 mkdir -p $outdir
 
