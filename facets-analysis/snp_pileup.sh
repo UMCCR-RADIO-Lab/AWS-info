8b7b99831f7c1f341cd195ab0b2c380ed033f29d
AndrewPattison <patto333333@gmail.com>
2020-09-30 16:51:42 +1000

    Swapped picard for samtools

--- facets-analysis/snp_pileup.sh
+++ facets-analysis/snp_pileup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 docker_cmd_prefix="docker run -it -v /scratch:/scratch -v /mnt/data:/mnt/data -v /mnt/refdata:/mnt/refdata -v /mnt/out:/mnt/out --env=TMP_DIR --env=JAVA_OPTS --memory=8G"
-picard="$docker_cmd_prefix quay.io/biocontainers/picard:2.23.3--0 picard $PICARD_JAVA_OPTIONS"
 snp_pileup="$docker_cmd_prefix quay.io/biocontainers/snp-pileup:0.5.14--hfbaaabd_3 snp-pileup"
+# Need to load in samtools
 
 # Example command
 # bash snp_pileup.sh hg19.fa blood.bam tumour.bam ouput_dir snp_reference
@@ -18,9 +18,10 @@ outdir=$4
 #00-All.vcf.gz
 SNP_reference=$5
 
-# BCBio has output files in the wrong order that FACETS wants for some reason. Picard reorder to fix this
-$picard ReorderSam -I $BCBio_tumour_bam -O $outdir/`basename $BCBio_tumour_bam`.ordered.bam -R $genome_ref -SD /mnt/refdata/genomes_1.7/GRCh37/GRCh37.dict
-$picard ReorderSam -I $BCBio_blood_bam -O $outdir/`basename $BCBio_blood_bam`.ordered.bam -R $genome_ref -SD /mnt/refdata/genomes_1.7/GRCh37/GRCh37.dict
+# BCBio has output files in the wrong order that FACETS wants for some reason. 
+# Samtools sort with 10 threads to hopefully fix this
+$samtools sort -@ 10 -o $outdir/`basename $BCBio_tumour_bam`.ordered.bam $BCBio_tumour_bam
+$samtools sort -@ 10 -o $outdir/`basename $BCBio_blood_bam`.ordered.bam $BCBio_blood_bam
 
 mkdir -p $outdir
 
