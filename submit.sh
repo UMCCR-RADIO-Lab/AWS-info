9347f07c6155aa698f0ccf3aaefa937aa2aca6b2
AWS ParallelCluster user <ec2-user@ip-10-2-0-124.ap-southeast-2.compute.internal>
2020-10-21 12:06:37 +1100

    Working now

--- submit.sh
+++ submit.sh
@@ -2,18 +2,19 @@
 #SBATCH --output %J.out
 #SBATCH --error %J.err
 #SBATCH --time=24:50:00
+#SBATCH --mem=29G
 
 # Source and activate conda
 . /home/ec2-user/.conda/etc/profile.d/conda.sh
 conda activate samtools
 
 #time
-#bash facets-analysis/snp_pileup.sh \
-#	/efs/refdata/hg38.fa \
-#        /efs/data/SBJ00605_PRJ200472_L2000846-ready.bam \
-#        /efs/data/SBJ00605_PRJ200504_L2000847-ready.bam \
-#        /efs/out \
-#	/efs/refdata/All_20170710.vcf.gz
+bash facets-analysis/snp_pileup.sh \
+	/efs/refdata/hg38.fa \
+        /efs/data/SBJ00605_PRJ200472_L2000846-ready.bam \
+        /efs/data/SBJ00605_PRJ200504_L2000847-ready.bam \
+        /efs/out \
+	/efs/refdata/All_20170710.vcf.gz
 
 
 time Rscript facets-analysis/Facets_R_part.R /efs/out/ 500
